8adc99a87359f4f588f2969312a5bf3a5b77b664
Michel <42797392+Misk77@users.noreply.github.com>
2018-10-30 14:08:13 +0100

    Project2
    Project2

---
+++ appett/webconnect.py
@@ -0,0 +1,27 @@
+import webbrowser
+
+
+def homeConnect(): \
+        webbrowser.open('https://miskplayground.000webhostapp.com/index.html', new=2)
+
+
+def googleConnect(): \
+        webbrowser.open('https://www.google.com/', new=2)
+
+
+def slackConnect(): \
+        webbrowser.open('https://slack.com/signin', new=2)
+
+
+def guiPthonkConnect(): \
+        webbrowser.open('https://www.tutorialspoint.com/python3/python_gui_programming.htm', new=2)
+
+
+def soloLearnConnect(): \
+        webbrowser.open('https://www.sololearn.com/', new=2)
+
+def unofficialWinBIN(): \
+        webbrowser.open('https://www.lfd.uci.edu/~gohlke/pythonlibs/', new=2)
+        #Unofficial Windows Binaries for Python Extension Packages
+
+
